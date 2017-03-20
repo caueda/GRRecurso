9cd7978d4a62f8c31ad0c53194e68a11375ce269
Carlos R. Ueda <carlos.ueda@gmail.com>
2017-03-20 08:50:36 -0400

    Refactoring imports

--- src/br/com/grrecurso/seguranca/spring/configuration/SpringLoginConfig.java
+++ src/br/com/grrecurso/seguranca/spring/configuration/SpringLoginConfig.java
@@ -1,19 +1,12 @@
 package br.com.grrecurso.seguranca.spring.configuration;
 
-import java.util.regex.Pattern;
-
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.util.matcher.RegexRequestMatcher;
-import org.springframework.security.web.util.matcher.RequestMatcher;
 
 import br.com.grrecurso.seguranca.spring.handlers.AutenticationFailureImpl;
 import br.com.grrecurso.seguranca.spring.handlers.AuthenticationSuccessImpl;
