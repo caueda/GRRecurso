e4e8c346cb90aef9deaceceb3c02f6793a03fcb9
Carlos R. Ueda <carlosueda@mti.mt.gov.br>
2017-04-28 18:40:51 -0400

    Vincular Permissões - Início

--- src/br/com/grrecurso/managed/usuario/PermissaoAction.java
+++ src/br/com/grrecurso/managed/usuario/PermissaoAction.java
@@ -7,19 +7,23 @@ import javax.inject.Named;
 
 import org.omnifaces.cdi.ViewScoped;
 
+import com.ocpsoft.pretty.faces.annotation.URLAction;
 import com.ocpsoft.pretty.faces.annotation.URLBeanName;
 import com.ocpsoft.pretty.faces.annotation.URLMapping;
 import com.ocpsoft.pretty.faces.annotation.URLMappings;
 
 import br.com.grrecurso.core.managed.AbstractManagedBean;
 import br.com.grrecurso.entities.usuario.Permissao;
+import br.com.grrecurso.entities.usuario.Role;
 import br.com.grrecurso.service.login.PermissaoService;
+import br.com.grrecurso.service.login.RoleService;
 
 @Named
 @ViewScoped
 @URLBeanName("permissaoAction")
 @URLMappings( mappings= {
 		@URLMapping(id="newPermissao", pattern="/app/permissao/#{tipoOperacao : permissaoAction.tipoOperacao}", viewId="/application/permissao/permissao.jsf"),
+		@URLMapping(id="vincularPermissao", pattern="/app/permissao/vincular/#{idRole : permissaoAction.idRole}", viewId="/application/permissao/vincularPermissao.jsf"),
 		@URLMapping(id="editPermissao", pattern="/app/permissao/#{tipoOperacao : permissaoAction.tipoOperacao}/id/#{idPermissao : permissaoAction.idPermissao}", 
 		onPostback=false, viewId="/application/permissao/permissao.jsf"),
 })
@@ -31,13 +35,25 @@ public class PermissaoAction extends AbstractManagedBean {
 	private static final long serialVersionUID = -1338935076679440203L;
 	private Permissao permissao;
 	private Long idPermissao;
+	private Long idRole;
+	private Role role;
 	
 	@EJB
 	private PermissaoService permissaoService;
 	
+	@EJB
+	private RoleService roleService;
+	
 	@PostConstruct
 	public void init() {
 		logger.info("[PermissaoAction.init] " + this.toString());
+	}
+	
+	@URLAction(mappingId="vincularPermissao", onPostback=false)
+	public void exibirVincularPermissao() {
+		if(getIdRole() != null) {
+			setRole(roleService.loadById(getIdRole()));
+		}
 	}
 	
 	@PreDestroy
@@ -95,5 +111,21 @@ public class PermissaoAction extends AbstractManagedBean {
 
 	public void setIdPermissao(Long idPermissao) {
 		this.idPermissao = idPermissao;
+	}
+
+	public Long getIdRole() {
+		return idRole;
+	}
+
+	public void setIdRole(Long idRole) {
+		this.idRole = idRole;
+	}
+
+	public Role getRole() {
+		return role;
+	}
+
+	public void setRole(Role role) {
+		this.role = role;
 	}
 }
